7b71f1ea5c2798ba3cb1f31087a306d56dcd5b62
ryoji-ishii <goccha.dev@gmail.com>
2025-11-26 15:05:14 +0900

    refactor Truncate function to support pagination with options for improved efficiency

--- pkg/batches/truncate.go
+++ pkg/batches/truncate.go
@@ -2,8 +2,10 @@ package batches
 
 import (
 	"context"
+
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/goccha/dynamodb-verse/pkg/foundations"
+	"github.com/goccha/dynamodb-verse/pkg/foundations/options"
 )
 
 type TruncateClient interface {
@@ -13,23 +15,35 @@ type TruncateClient interface {
 
 // Truncate Deletes records in the specified table.
 func Truncate[T any](ctx context.Context, db TruncateClient, condition foundations.ScanFilterFunc, keyFunc DeleteKeyFunc[T]) error {
-	b := New()
-	_, err := foundations.ScanAll(ctx, db, condition, func(tableName string, value foundations.Records) error {
-		rec := make([]T, len(value))
-		if err := value.Unmarshal(ctx, &rec); err != nil {
+	var opt []options.Option
+	for {
+		b := New()
+		out, err := foundations.Scan(ctx, db, condition, func(tableName string, value foundations.Records) error {
+			rec := make([]T, len(value))
+			if err := value.Unmarshal(ctx, &rec); err != nil {
+				return err
+			}
+			for _, v := range rec {
+				b.Delete(func() (table string, item map[string]types.AttributeValue, err error) {
+					table = tableName
+					item = keyFunc(v)
+					return
+				})
+			}
+			return nil
+		}, opt...)
+		if err != nil {
 			return err
 		}
-		for _, v := range rec {
-			b.Delete(func() (table string, item map[string]types.AttributeValue, err error) {
-				table = tableName
-				item = keyFunc(v)
-				return
-			})
+		if out.LastEvaluatedKey != nil {
+			opt = []options.Option{options.ExclusiveStartKey(out.LastEvaluatedKey)}
 		}
-		return nil
-	})
-	if err != nil {
-		return err
+		if err = b.Run(ctx, db); err != nil {
+			return err
+		}
+		if len(opt) == 0 {
+			break
+		}
 	}
-	return b.Run(ctx, db)
+	return nil
 }
